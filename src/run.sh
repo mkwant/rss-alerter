247d939d3486c8b178d0cbb83fe4ba646da8bd38
Maarten Kwant <mkwant@xs4all.nl>
2026-03-06 23:29:04 +0100

    Removed superfluous -it flag

--- src/run.sh
+++ src/run.sh
@@ -2,8 +2,9 @@
 arguments=( "$@" )
 
 cd /var/lib/rss-alert || exit
-/bin/docker run -it --rm --quiet \
+/bin/docker run --rm --quiet \
     --pull always \
     --env-file .env \
+    -v "/var/log/rss-alert:/app/logs" \
     -v "$(pwd)/history:/app/history" \
     ghcr.io/mkwant/rss-alerter:latest uv run src/rss_alert/main.py "${arguments[@]}"
